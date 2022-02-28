4bc2e89cfc70c516de5549cca7ab97a2f36f7337
RonTuretzky <74178515+RonTuretzky@users.noreply.github.com>
2022-02-28 15:32:33 +0200

    Update CommunityToken.sol

--- CommunityToken.sol
+++ CommunityToken.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.0;
 
 interface CommunityToken{
 
-    function add_event(uint256 event_id_, address host) external view;
-    function verify_attendence(uint256 event_id_, address attendee) external view;
-    function increment_count(address attendee) external view;
-    function add_user(address new_user_) external view;
+    function add_event(uint256 event_id_, address host) public returns(bool);
+    function verify_attendence(uint256 event_id_, address attendee) public returns(bool);
+    function increment_count(address attendee) public returns(bool);
+    function add_user(address new_user_) public returns(bool);
 
 
 
